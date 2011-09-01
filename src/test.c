a04df0418f49165045379d1431dc6450ccefd193
Alan Medlar <amedlar@gmail.com>
2011-09-01 01:05:15 +0100

    more noodling around in C

--- src/test.c
+++ src/test.c
@@ -1,44 +1,22 @@
-/*
-import sys
-
-nodes = [1,2,3]
-values = map(lambda x : 4 ** x, range(len(nodes)))
-
-def index2assignment(ind) :
-    index = ind
-    tmp = [0] * len(values)
-    for i in sorted(range(len(values)), reverse=True) :
-        tmp[i] = index / values[i]
-        index = index % values[i]
-    return tmp
-
-def assignment2index(a) :
-    tmp = 0
-    for i in range(len(a)) :
-        tmp += (a[i] * values[i])
-    return tmp
-
-
-x = eval(sys.argv[1])
-
-index = assignment2index(x)
-newx = index2assignment(index)
-
-print "%s\n%s\n%s" % (x, index, newx)
-*/
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
 
+#define NUM_NODES 4
 #define NUM_ALLELES 4
 
-#define TRAIT_AA 0
-#define TRAIT_BA 1
-#define TRAIT_AB 2
-#define TRAIT_BB 3
+enum { 
+    TRAIT_AA,
+    TRAIT_BA,
+    TRAIT_AB,
+    TRAIT_BB
+};
 
-#define NUM_NODES 4
+enum {
+    CHILD_PEEL,
+    PARTNER_PEEL,
+    PARENT_PEEL,
+};
 
 int offsets[] = {
     1 <<  0, 
@@ -54,23 +32,99 @@ int offsets[] = {
 };
 
 struct rfunction {
-    int* cutset;                // eg: [1,2,3]
-    int  length;
+    int peel_node;
+    int peel_type;
     
-    float* matrix;              // len = length ** NUM_ALLELES
+    // THE 'peel_node' MUST ALWAYS BE LOCATED
+    // AT cutset[cutset_length - 2], SO IT CAN 
+    // BE IGNORED EASILY
+    
+    int* cutset;                // eg: [1,2,3] (including the 'peel_node')
+    int cutset_length;
+    
+    // how are these two matrices going to be treated?
+    // could the index to one be a factor of the other
+    // eg: presum_matrix[(x,y,z)] and matrix[(x,y,z) / z]?
+    
+    float* presum_matrix;
+    int presum_length;          // 4 ** (cutset_length - 1)
+    
+    float* matrix;
+    int matrix_length;          // 4 ** cutset_length
     
     struct rfunction* prev1;    // must be NULL if not used
-    struct rfunction* prev2;    // must be NULL if not used
-    
-    int* cutset_indices_prev1;
-    int  length_prev1;
-    int* cutset_indices_prev2;
-    int  length_prev2;
+    struct rfunction* prev2;    // must be NULL if not used    
 };
+
+// XXX
+// get, set, add, zero are a bad idea perhaps, because they
+// always assume a certain matrix...
+//
+void rfunction_zero(struct rfunction* rf) {
+    int i;
+    
+    for(i = 0; i < rf->matrix_length; ++i) {
+        rf->matrix[i] = 0.0;
+    }
+}
 
 double rfunction_get(struct rfunction* rf, int index) {
     return rf->matrix[index];
 }
+
+void rfunction_set(struct rfunction* rf, int index, float val) {
+    rf->matrix[index] = val;
+}
+
+void rfunction_add(struct rfunction* rf, int index, float val) {
+    rf->matrix[index] += val;
+}
+
+// I am going to assume that the length of 'assignment' is to the number of 
+// pedigree members and that everything that is not assigned is -1
+//
+int rfunction_index(struct rfunction* rf, int* assignment, int length) {
+    int i, tmp = 0;
+    
+    for(i = 0; i < rf->cutset_length; ++i) {
+        tmp += (assignment[rf->cutset[i]] * offset[i]);
+    }
+    
+    return tmp;
+}
+
+// this works out the index for the 'presum_matrix', though with this
+// scheme I can always just do:
+//      rf->matrix[index - ((0..3) * offset[rf->cutset_length - 1])]
+// when I sum over one of the dimensions so long as I ensure that
+// the peel_node is always in cutset[cutset_length - 1]
+int rfunction_presum_index(struct rfunction* rf, int* assignment, int length) {
+    int i, tmp = 0;
+    
+    for(i = 0; i < (rf->cutset_length - 1); ++i) {
+        tmp += (assignment[rf->cutset[i]] * offset[i]);
+    }
+    
+    return tmp;
+}
+
+// from an index, construct the assignment of genotypes to cutset nodes
+// 
+void rfunction_assignment(struct rfunction* rf, int ind, int* assignment, int length) {
+    int index = ind;
+    int i;
+    
+    for(i = 0; i < length; ++i) {
+        assignment[i] = -1;
+    }
+    
+    for(i = (rf->cutset_length - 1); i > -1; --i) {
+        assignment[rf->cutset[i]] = index / offsets[i];
+        index %= offsets[i];
+    }
+}
+
+// -----
 
 void index2assignment(int ind, int* values, int length) {
     int index = ind;
@@ -88,17 +142,6 @@ int assignment2index(int* values, int length) {
     
     for(i = 0; i < length; ++i) {
         tmp += (values[i] * offsets[i]);
-    }
-    
-    return tmp;
-}
-
-int assignment2index2(int* values, int length, int* indices, int indices_length) {
-    int tmp = 0;
-    int i;
-    
-    for(i = 0; i < indices_length; ++i) {
-        tmp += (values[indices[i]] * offsets[i]);
     }
     
     return tmp;

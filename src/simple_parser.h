9934caa46d6437cf2fe96efd762daa07bfe6772b
Alan Medlar <ajm@oak.lan>
2012-04-05 00:43:09 +0300

    added simple parser, necessary for previous commit

---
+++ src/simple_parser.h
@@ -0,0 +1,51 @@
+#ifndef LKG_SIMPLEPARSER_H_
+#define LKG_SIMPLEPARSER_H_
+
+#include <vector>
+#include <string>
+#include <cstdio>
+
+#include "parser.h"
+
+
+class SimpleParser : public Parser {
+
+    vector<unsigned int> data;
+
+ public :
+	SimpleParser(const string fn) : 
+	    Parser(fn, false),
+		data() {}
+    
+	bool parse_line(const int linenum, const string s) {
+		tokenise(s);
+		
+		if(tokens.size() != 1) {
+		    fprintf(stderr, 
+		            "error: read more than one field on line %d in peel sequence file %s (read \"%s\")\n", 
+		            linenum, filename.c_str(), s.c_str());
+		    return false;
+		}
+		
+		unsigned int tmp;
+		stringstream ss(tokens[0]);
+		
+		if((ss >> tmp).fail()) {
+		    fprintf(stderr, 
+		            "error: not an integer, line %d, peel sequence file %s (read \"%s\")\n", 
+		            linenum, filename.c_str(), s.c_str());
+		    return false;
+		}
+		
+		data.push_back(tmp);
+		
+		return true;
+	}
+	
+	vector<unsigned int>& get_values() {
+	    return data;
+	}
+};
+
+#endif
+

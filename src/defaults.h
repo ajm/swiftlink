fc837f51d373f07f0035b6f90669958da49c5707
Alan Medlar <ajm@1BQQCSL.viikki.dhcp.hupnet.helsinki.fi>
2016-08-29 17:07:10 +0300

    change of defaults

--- src/defaults.h
+++ src/defaults.h
@@ -1,14 +1,14 @@
 #ifndef LKG_DEFAULTS_H_
 #define LKG_DEFAULTS_H_
 
-const double DEFAULT_ELOD_FREQUENCY         = 0.00001;
+const double DEFAULT_ELOD_FREQUENCY         = 0.0001;
 const double DEFAULT_ELOD_PENETRANCE[3]     = { 0.0, 0.0, 1.0 };
 const double DEFAULT_ELOD_SEPARATION        = 0.05;
 const int DEFAULT_ELOD_REPLICATES           = 1000000;
 
 const int DEFAULT_MCMC_CHAINS               = 1;
-const int DEFAULT_MCMC_ITERATIONS           = 90000;
-const int DEFAULT_MCMC_BURNIN               = 10000;
+const int DEFAULT_MCMC_ITERATIONS           = 80000;
+const int DEFAULT_MCMC_BURNIN               = 20000;
 const int DEFAULT_MCMC_EXCHANGE_PERIOD      = 10;
 const int DEFAULT_MCMC_SCORING_PERIOD       = 10;
 const int DEFAULT_MCMC_RUNS                 = 1;
